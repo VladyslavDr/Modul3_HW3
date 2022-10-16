b39d82faa1530edba0c1b25be89befa129e38a5e
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-16 17:30:39 +0300

    Done

---
+++ Task/Task/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Class1
+    {
+        public Action<bool> Action => Program.Show;
+        public int Pow(int x, int y) => x * y;
+    }
+}
